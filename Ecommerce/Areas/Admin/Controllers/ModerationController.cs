04f3012d812cc7844e0b06419566f4bc5e3b2e42
Abdalkrem2 <abdalkremalshbol8@gmail.com>
2026-08-22 16:23:21 +0300

    phase 5 done

---
+++ Ecommerce/Areas/Admin/Controllers/ModerationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Areas.Admin.Controllers
+{
+    public class ModerationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
