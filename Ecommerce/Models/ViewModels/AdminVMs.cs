3eae26a391d5a979c3cdaf34d7604b0752a305ed
Haneen Mohammad <haneenmoh775@gmail.com>
2026-08-22 14:36:39 +0300

    anY

--- Ecommerce/Models/ViewModels/AdminVMs.cs
+++ Ecommerce/Models/ViewModels/AdminVMs.cs
@@ -14,9 +14,9 @@ namespace Ecommerce.Models.ViewModels
     public class CategoryVM
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string ImagePath { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
         public int ProductCount { get; set; }
     }
 
@@ -24,13 +24,15 @@ namespace Ecommerce.Models.ViewModels
     {
         public int Id { get; set; }
 
-        [Required, StringLength(100)]
-        public string Name { get; set; }
+        [Required(ErrorMessage = "Category name is required.")]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
 
         [StringLength(500)]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public IFormFile ImageFile { get; set; }
-        public string ExistingImagePath { get; set; }
+        public IFormFile? ImageFile { get; set; }
+
+        public string? ExistingImagePath { get; set; }
     }
 }
